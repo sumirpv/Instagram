3c11336234455c2a6398ef4211b92e7bc55cbaa5
sumirpv <sumirpv@gmail.com>
2018-10-25 22:13:58 -0700

    test

---
+++ src/components/photo-feed.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {  ScrollView } from 'react-native';
+import axios from 'axios';
+import PhotoSection from './photo-section'
+export default class PhotoFeed extends Component {
+    state = { photos: ['']};
+    componentDidMount(){
+        axios.get('http://localhost:3000/photos')
+        .then (res => {
+          this.setState({ photos:res.data})
+          });
+    }
+
+    getPhoto(){
+        return this.state.photos.map(myPhoto =>{
+            return(
+                <PhotoSection photo ={myPhoto} />
+            );
+        })
+    }
+  render() {
+    return (
+      <ScrollView Horizontal = { true }>
+      {this.getPhoto()}
+      </ScrollView>
+    );
+  }
+}
---
+++ src/components/photo-section.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { View, Text, Image,  Button } from 'react-native';
+import IonIcon from 'react-native-vector-icons/Ionicons';
+
+export default class PhotoSection extends Component {
+  render() {
+    return (
+      <View style= {styles.container}>
+          <View style ={styles.thumbNailSection}>
+             <Image style= {styles.thumbNail}
+             source={{uri:this.props.photo.user_avatar}}/>
+             <View style = { styles.userContainer}>
+             <Text  style ={styles.userName }>
+             {this.props.photo.username}
+             </Text> 
+             </View>
+          </View>
+          <View>
+              <Image style= {{width:null, height:200}}
+              source={{uri:this.props.photo.image}}/>
+          </View>
+          <View style ={ styles.heartContainer}>
+              <IonIcon
+              name='ios-heart-outline'
+              size={30} />
+          </View>
+          <View style= { styles.imageMeta}>
+              <Text style ={styles.userName }>User Name</Text>
+              <Text>{this.props.photo.caption}</Text>
+          </View>
+      </View>
+    );
+  }
+}
+const styles ={
+    container :{
+        margin: 10,
+    },
+    thumbNailSection:{
+        flexDirection: 'row',
+        padding: 5,
+        borderBottomColor: '#ddd',
+        borderBottomWidth: 1,
+    },
+    thumbNail:{
+        width: 50, 
+        height : 50, 
+        borderRadius: 25,
+    },
+    userContainer:{
+        justifyContent: 'center',
+        padding:5,
+    },
+    imageMeta:{
+        flexDirection: 'row',
+    },
+    userName:{
+        fontWeight: 'bold',
+        paddingRight: 5,
+    },
+    heartContainer:{
+        paddingTop: 10,
+        paddingBottom: 10,
+    }
+}
